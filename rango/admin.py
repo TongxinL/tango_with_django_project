45dff3b813d1bddc5c8e43e06eb03b45da5b5308
Tongxin Li <2534975L@student.gla.ac.uk>
2021-07-29 01:37:48 +0100

    chapter 9.5

--- rango/admin.py
+++ rango/admin.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 
 from django.contrib import admin
-
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 # Add in this class to customise the Admin Interface
@@ -17,3 +16,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
